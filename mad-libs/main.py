3f429e000e6fb648725d67f9ba8c5db580963fb6
Yairys <yayi.arist.hz@gmail.com>
2022-10-09 20:19:04 -0300

    mad libs

---
+++ mad-libs/main.py
@@ -0,0 +1,13 @@
+#Mad Libs (historias locas)
+adj1 = input("adjective: ")
+adj2 = input("adjective: ")
+bird = input("Type o f bird: ")
+room = input("room in a house: ")
+verb1 = input("room in a house: ")
+verb2 = input("room in a house: ")
+
+
+madlib = f"It was a {adj1}, cold November day. I woke up to the {adj2} smell of {bird} roasting in the {room} downstairs. i {verb1} down the stairs to see if I could help {verb2} the dinner. "
+
+print(madlib)
+
